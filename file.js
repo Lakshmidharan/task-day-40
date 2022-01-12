560184d04df25d42627c48360b9f85708df63937
Lakshmidharan <lakshmidharan2401@gmail.com>
2022-01-11 20:09:39 -0800

    changes made

--- file.js
+++ file.js
@@ -9,24 +9,34 @@ let filename=`${time}.txt`
 let timestamp=date.getTime().toString();
 
 
-
 let option={
     origin:"*"
 }
 app.use(cors(option))
 
-
-
-
     app.get("/", function (req, res) {
+        let date= new Date();
+        let time=`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}-${date.getHours()}-${date.getMinutes()}`
+        let filename=`${time}.txt`
+        let timestamp=date.getTime().toString();
         res.json(
-            fs.writeFile(`./files.${filename}`,timestamp ,function (err, data) {
+            fs.writeFile(`./folder/${filename}`,timestamp ,function (err, data) {
                 if (err) throw err;
-                console.log("filename");
+                console.log("File Created");
 
             })
 
         )
+    });
 
+    app.get("/", function (req, res) {
+        res.json(
+            fs.readdir("./folder" ,function (err,data) {
+                if (err) throw err;
+                console.log(data);
+
+            })
+
+        )
     });
 app.listen(3000)
